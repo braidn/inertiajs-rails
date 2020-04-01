bf6b38d7a0d64e5931854137d9dc886602c14218
Braden Douglass <braden.douglass@gmail.com>
2020-04-01 14:39:26 -0400

    Implement Show URL For Products.
    To be able to one click buy a product, there needs to be some kind of
    product display page or 'PDP'. Implement this in the products controller
    and pass the relevant information surrounding the product to Inertia.
    This page doesn't exist yet so the product system test around this route
    will fail (not committed) but, the integration test for the controller
    does pass.
    
    This also includes a few bits of cleanup to bring the product
    integration test inline with minitest spec versus the assertion
    matchers.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
   def index
     render inertia: 'Products/Index', props: {
-      products: Product.all.as_json
+      products: Product.all.map do |product|
+        product.as_json.merge(
+          self: { href: product_url(product) }
+        )
+      end
+    }
+  end
+
+  def show
+    render inertia: 'Products/Show', props: {
+      product: Product.find(params[:id])
     }
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  it "succesfully get's the index" do
+  it "succesfully renders a list of resources" do
     get products_url
 
-    assert_response :success
+    _(response.code).must_equal '200'
+  end
+
+  it 'succesfully renders a single resource' do
+    get product_url(products(:one))
+
+    _(response.code).must_equal '200'
   end
 end
